0243889acc9d5703a524a42f92e94a9d0d76e2f7
GrittyB <74409416+GrittyB@users.noreply.github.com>
2023-04-11 23:25:16 +0800

    update

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,12 +1,11 @@
 // move_semantics1.rs
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0); // 这里把vec0传入了,所有权转移, 因为没有Copy, 之后也不能使用vec0了
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -16,7 +15,8 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    // 这里不是产生了一个新的vec, 只是所有权转移了, 旧的vec的所有权消失
+    let mut vec = vec; 
 
     vec.push(22);
     vec.push(44);
