5743e33fc338a7ea66ab732819e2b7457284b82f
GrittyB <74409416+GrittyB@users.noreply.github.com>
2023-04-17 22:11:03 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,38 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    
+    // 实现trim
+    let mut start = 0;
+    let mut end = input.len();
+    for (i, c) in input.char_indices() {
+        if !c.is_whitespace() {
+           start = i;
+           break; 
+        }
+    }
+    for (i, c) in input.char_indices().rev() {
+        if !c.is_whitespace() {
+            end = i + c.len_utf8();
+           break; 
+        }
+    }
+    (&input[start..end]).to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut output = input.to_string();
+    output.push_str(" world!");
+    output
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let output = input.to_string();
+    output.replace("cars", "balloons")
 }
 
 #[cfg(test)]
